79d1479253f434129f2f537fbacc2b36b6e44a9a
agrozyme <agrozyme@gmail.com>
2018-03-22 13:31:14 +0800

    remove index

--- Extend/index.ts
+++
@@ -1,5 +0,0 @@
-export {default as Skill} from './Skill';
-export {default as Provider} from './Provider';
-export {default as Request} from './Request';
-export {default as Intent} from './Intent';
-
